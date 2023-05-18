be5dabdcb99344308b39fa76ef2529f01fdbdd04
Alvaro Blanco <alvaroblanco10@gmail.com>
2023-05-18 17:11:26 -0300

    load fix y load movil elements

--- uber.py
+++ uber.py
@@ -14,6 +14,85 @@ def loadMap(dir:str):
             pickle.dump(myMap,gm)
     except Exception as e:
         print(e)
+
+def load_fix_element(element: str, direction: str):
+    # Verificar si el nombre del elemento es válido
+    if not element.isalnum():
+        print('Error: El nombre del elemento solo puede contener caracteres alfanuméricos.')
+        return
+    
+    # Definir el diccionario de correspondencias entre nombres y códigos
+    name_mapping = {
+        'H': 'hospital',
+        'A': 'almacén',
+        'T': 'tienda',
+        'S': 'supermercado',
+        'E': 'escuela',
+        'K': 'kiosco',
+        'I': 'iglesia'
+    }
+    
+    # Analizar la dirección y extraer los datos
+    pattern = r'<([a-zA-Z]+), (\d+)>'
+    matches = re.findall(pattern, direction)
+    if len(matches) != 2:
+        print('Error: La dirección debe consistir en dos tuplas.')
+        return
+    
+    # Crear las tuplas de dirección
+    dir1 = (matches[0][0], int(matches[0][1]))
+    dir2 = (matches[1][0], int(matches[1][1]))
+    
+    # Verificar si el nombre del elemento ya está en uso
+    if element in name_mapping.values():
+        print(f'Error: El nombre del elemento "{element}" ya está en uso.')
+        return
+    
+    # Crear el elemento fijo y mostrar información
+    nombre = name_mapping.get(element, 'Desconocido')
+    fijo = elmFijo(nombre, [dir1, dir2])
+    print(f'Se ha cargado el elemento fijo: {fijo.nombre} - {fijo.direccion}')
+
+def load_movil_element(element: str, direction: str, monto: str):
+    # Verificar si el nombre del elemento es válido
+    if not element.isalnum():
+        print('Error: El nombre del elemento solo puede contener caracteres alfanuméricos.')
+        return
+    
+    # Definir el diccionario de correspondencias entre nombres y códigos
+    name_mapping = {
+        'P': 'persona',
+        'C': 'auto'
+    }
+    
+    # Analizar la dirección y extraer los datos
+    pattern = r'<([a-zA-Z]+), (\d+)>'
+    matches = re.findall(pattern, direction)
+    if len(matches) != 2:
+        print('Error: La dirección debe consistir en dos tuplas.')
+        return
+    
+    # Crear las tuplas de dirección
+    dir1 = (matches[0][0], int(matches[0][1]))
+    dir2 = (matches[1][0], int(matches[1][1]))
+    
+    # Verificar si el nombre del elemento ya está en uso
+    if element in name_mapping.values():
+        print(f'Error: El nombre del elemento "{element}" ya está en uso.')
+        return
+    
+    # Verificar si el monto es un valor numérico
+    try:
+        monto = float(monto)
+    except ValueError:
+        print('Error: El monto debe ser un valor numérico.')
+        return
+    
+    # Crear el elemento móvil y mostrar información
+    nombre = name_mapping.get(element, 'Desconocido')
+    movil = elmMovil(nombre, [dir1, dir2], monto)
+    print(f'Se ha cargado el elemento móvil: {movil.nombre} - {movil.direccion} - Monto: {movil.monto}')
+
 class mapa():
     def fhash(self,key):
         return int(key[1:])-1
@@ -63,9 +142,11 @@ if __name__=="__main__":
     if args["create_map"]!=None:
         print(args["create_map"])
         loadMap(args["create_map"])
-    if args["load_fix_element"]!=None:
-        print(args["load_fix_element"])
-    if args["load_movil_element"]!=None:
-        print(args["load_movil_element"])
+    if args["load_fix_element"] != None:
+        element, direction = args["load_fix_element"].split(',')
+        load_fix_element(element.strip(), direction.strip())
+    if args["load_movil_element"] != None:
+        element, direction, monto = args["load_movil_element"].split(',')
+        load_movil_element(element.strip(), direction.strip(), monto.strip())
     if args["create_trip"]!=None:
         print(args["create_trip"])
