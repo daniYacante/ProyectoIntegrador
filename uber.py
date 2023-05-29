c30672ae6b980aef5f249c6dd795e5d966bdfa32
daniYacante <danizyacante@gmail.com>
2023-05-29 18:09:45 -0300

    loadElm

--- uber.py
+++ uber.py
@@ -31,8 +31,13 @@ def loadMap(dir:str):
     if serializar(myMap,"grafMap"):
         print("Map created successfully")
 def loadElm(element:str):
-    test=re.findall("\w+,{\w+}",element)
-    elm=element.split(',')
+    # Expresion regular que encuentra el objeto y las dos partes de la direccion
+    patt=re.compile("([A-Z]\d+),{(<.*>),(<.*>)},*(\d+)*")
+    mat=re.search(patt,element)
+    elm=mat.group(1,2,3)
+    # print(mat.group(1))
+    # print(mat.group(2))
+    # print(mat.group(3))
     myMap=deserializar("grafMap")
     if myMap==None:
         return
@@ -40,45 +45,55 @@ def loadElm(element:str):
     if not elm[0].isalnum():
         print('Error: El nombre del elemento solo puede contener caracteres alfanuméricos.')
         return
-    elemento=element[0]
-    direction=element[1]
+    # elemento=element[0]
+    # direction=element[1]
     # Analizar la dirección y extraer los datos
-    pattern = r'<([a-zA-Z]+), (\d+)>'
-    matches = re.findall(pattern, direction)
-    if len(matches) != 2:
+    # pattern = r'<([a-zA-Z]+), (\d+)>'
+    # matches = re.findall(pattern, direction)
+    if len(elm) != 3:
         print('Error: La dirección debe consistir en dos tuplas.')
         return
     # Crear las tuplas de dirección
-    dir1 = (matches[0][0], int(matches[0][1]))
-    dir2 = (matches[1][0], int(matches[1][1]))
+    # dir1 = (matches[0][0], int(matches[0][1]))
+    # dir2 = (matches[1][0], int(matches[1][1]))
+    dir1=elm[1]
+    dir2=elm[2]
+    if not myMap.checkDir(dir1,dir2):
+        print("Direccion no valida")
+        return
     # Verificar si el nombre del elemento ya está en uso
-    if elemento in myMap.fijos or element in myMap.moviles:
+    fij=myMap.getFijos()
+    mov=myMap.getMoviles()
+    if elm[0] in fij.keys() or elm[0] in mov.keys():
         print(f'Error: El nombre del elemento "{element}" ya está en uso.')
         return
-    if len(elm)==3:
+    #Si existe monto es un objeto movil
+    if mat.group(4)!=None:
         # Verificar si el monto es un valor numérico
         try:
-            monto = float(monto)
+            monto = float(mat.group[4])
         except ValueError:
             print('Error: El monto debe ser un valor numérico.')
             return
         # Crear el elemento móvil y mostrar información
-        movil = elmMovil(elemento, [dir1, dir2], monto)
+        movil = elmMovil(elm[0], [dir1, dir2], monto)
         # Actualizar el conjunto de nombres utilizados
-        myMap.moviles[elemento]=movil
+        # myMap.moviles[elm[0]]=movil
+        myMap.load(movil)
         print(f'Se ha cargado el elemento móvil: {movil.nombre} - {movil.direccion} - Monto: {movil.monto}')
-    elif len(elm)==2:
-        fijo = elmFijo(elemento, [dir1, dir2])
+    else:
+        fijo = elmFijo(elm[0], [dir1, dir2])
         # Actualizar el conjunto de nombres utilizados
-        myMap.fijos[elemento]=fijo
+        # myMap.fijos[elm[0]]=fijo
+        myMap.load(fijo)
         print(f'Se ha cargado el elemento fijo: {fijo.nombre} - {fijo.direccion}')
     return serializar(myMap,"grafMap")
 
 class mapa():
     def __init__(self,E:list,V:list) -> None:
         self.esquinas:Dict[esquina] = {}
-        self.fijos:Dict[elmFijo]
-        self.moviles:Dict[elmMovil]
+        self.fijos:Dict[elmFijo]={}
+        self.moviles:Dict[elmMovil]={}
         for vert in E:
             newVert=esquina(vert)
             self.esquinas[vert]=newVert
@@ -92,7 +107,31 @@ class mapa():
             self.fijos[elemento.nombre]=elemento
         else:
             self.moviles[elemento.nombre]=elemento
-        
+        # print(elemento.direccion)
+        ex=self.getGroups(elemento.direccion[0])
+        ey=self.getGroups(elemento.direccion[1])
+        calles=self.esquinas[ex.group(1)].vecinas
+        if ey.group(1) in calles.keys():
+            calles[ey.group(1)].elementos[elemento.nombre]=elemento
+        if self.checkDir(ey,ex):
+            calles=self.esquinas[ey.group(1)].vecinas
+            calles[ex.group(1)].elementos[elemento.nombre]=elemento
+    def getFijos(self):
+        return self.fijos
+    def getMoviles(self):
+        return self.moviles
+    def getGroups(self,dire):
+        d=re.search("(e\d+),(\d+)",dire)
+        return d
+    def checkDir(self,ex,ey):
+        d1=self.getGroups(ex)
+        d2=self.getGroups(ey)
+        l=int(d1.group(2))+int(d2.group(2))
+        calles=self.esquinas[d1.group(1)].vecinas
+        if d2.group(1) in calles.keys() and l==int(calles[d2.group(1)].distancia):
+            return True
+        else:
+            return False
 class esquina():
     def __init__(self,name:str) -> None:
         self.name=name
